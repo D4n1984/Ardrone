87ab462e2439186dbcdb8815ace19d56d1d7559c
D4n1984 <daniel.garcia.garcia.84@gmail.com>
2015-01-18 22:31:15 +0100

    Ampliación controles
    Se amplían los controles del vehículo para controlar todas las
    causísticas.

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,14 +1,72 @@
-int ledPin = 2;                 // LED connected to digital pin 13
+int pinRight = 3;                 
+int pinLeft = 2;
+int pinBack= 1;
+int pinFront = 0;
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+  pinMode(pinRight, OUTPUT);      
+  pinMode(pinLeft, OUTPUT);
+  pinMode(pinBack, OUTPUT);
+  pinMode(pinFront, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(ledPin, HIGH);   // sets the LED on
-  delay(2000);                  // waits for a second
-  digitalWrite(ledPin, LOW);    // sets the LED off
-  delay(2000);                  // waits for a second
+  turnRight();   
+  delay(2000);                  
+  turnLeft();    
+  delay(2000);
+  stopTurn();
+  delay(2000);
+  goBack();   
+  delay(2000);                  
+  goFront();    
+  delay(2000);
+  stopGo();
+  delay(2000);  
+  turnLeft();
+  goFront();
+  delay(2000);
+  turnRight();
+  delay(2000);
+  stopAll();
+  delay(4000);
+}
+
+void turnRight(){
+ digitalWrite(pinLeft, LOW);
+ digitalWrite (pinRight, HIGH);
+}
+
+void turnLeft(){
+  digitalWrite(pinRight, LOW);
+  digitalWrite(pinLeft, HIGH);
+}
+
+void stopTurn(){
+  digitalWrite(pinRight, LOW);
+  digitalWrite(pinLeft, LOW);
+}
+
+void goFront(){
+  digitalWrite(pinBack, LOW);
+  digitalWrite(pinFront, HIGH);
+}
+
+void goBack(){
+  digitalWrite(pinFront, LOW);
+  digitalWrite(pinBack, HIGH);
+}
+
+void stopGo(){
+  digitalWrite(pinBack, LOW);
+  digitalWrite(pinFront, LOW);
+}
+
+void stopAll(){
+  digitalWrite(pinLeft, LOW);
+  digitalWrite(pinRight, LOW);
+  digitalWrite(pinBack, LOW);
+  digitalWrite(pinFront, LOW);
 }
